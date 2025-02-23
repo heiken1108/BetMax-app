260692d85df05a779b34636251bd37f85059a417
Håkon Hoelsæter <hakon1108@gmail.com>
2025-02-23 23:22:17 +0100

    Made tile look nicer

--- lib/presentation/widget/match_card_tile.dart
+++ lib/presentation/widget/match_card_tile.dart
@@ -4,127 +4,317 @@ import 'package:intl/intl.dart';
 
 class MatchCardTile extends StatelessWidget {
   final MatchModel match;
-  MatchCardTile({required this.match});
+
+  const MatchCardTile({
+    super.key,
+    required this.match,
+  });
+
+  Color _getEvColor(double value) {
+    return value > 1 ? Colors.green.shade600 : Colors.red.shade600;
+  }
 
   @override
   Widget build(BuildContext context) {
-    Color evHomeColor =
-        match.expectedValue.home > 1 ? Colors.green : Colors.red;
-    Color evDrawColor =
-        match.expectedValue.draw > 1 ? Colors.green : Colors.red;
-    Color evAwayColor =
-        match.expectedValue.away > 1 ? Colors.green : Colors.red;
+    return Card(
+      elevation: 2,
+      margin: const EdgeInsets.all(12),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Header - Tournament and Time
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  match.tournament,
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  DateFormat('HH:mm dd/MM').format(match.startTime),
+                  style: const TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
 
-    return Container(
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black26, blurRadius: 4, offset: Offset(0, 2))
-          ]),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(match.tournament),
-              Text(DateFormat('HH:mm dd/MM').format(match.startTime))
-            ],
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Flexible(
-                child: Text(
-                    '${match.homeTeam} (${match.elo.homeElo.toStringAsFixed(1)})',
-                    softWrap: true,
-                    overflow: TextOverflow.clip),
-              ),
-              Flexible(
-                child: Text(
-                    '${match.awayTeam} (${match.elo.awayElo.toStringAsFixed(1)})',
-                    softWrap: true,
-                    overflow: TextOverflow.clip),
-              )
-            ],
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text('Sannsynligheter:'),
-            ],
-          ),
-          SizedBox(height: 10),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Text('${(match.elo.eloProbs.home * 100).toStringAsFixed(2)}%'),
-            Text('${(match.elo.eloProbs.draw * 100).toStringAsFixed(2)}%'),
-            Text('${(match.elo.eloProbs.away * 100).toStringAsFixed(2)}%')
-          ]),
-          SizedBox(height: 8),
-          // Horisontal bar for å visualisere prosentene
-          Row(
-            children: [
-              // Home bar
-              Expanded(
-                flex: (match.elo.eloProbs.home * 100)
-                    .toInt(), // Skalerer bredden basert på prosentandel
-                child: Container(
-                  height: 20,
-                  color: Colors.green,
+            // Teams
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        match.homeTeam,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        'ELO: ${match.elo.homeElo.toStringAsFixed(1)}',
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              // Draw bar
-              Expanded(
-                flex: (match.elo.eloProbs.draw * 100).toInt(),
-                child: Container(
-                  height: 20,
-                  color: Colors.grey,
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12),
+                  child: Text(
+                    'vs',
+                    style: TextStyle(color: Colors.grey),
+                  ),
                 ),
-              ),
-              Expanded(
-                flex: (match.elo.eloProbs.away * 100).toInt(),
-                child: Container(
-                  height: 20,
-                  color: Colors.red,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        match.awayTeam,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        'ELO: ${match.elo.awayElo.toStringAsFixed(1)}',
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
+              ],
+            ),
+            const SizedBox(height: 20),
+
+            // Probabilities
+            const Text(
+              'Win Probabilities',
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
               ),
-            ],
-          ),
-          SizedBox(height: 15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text('HUB Norsk Tipping:'),
-            ],
-          ),
-          SizedBox(height: 10),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Text('${match.odds.home}'),
-            Text('${match.odds.draw}'),
-            Text('${match.odds.away}')
-          ]),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text('Forventingsverdi pr. krone:'),
-            ],
-          ),
-          SizedBox(height: 10),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Text(match.expectedValue.home.toStringAsFixed(3),
-                style: TextStyle(color: evHomeColor)),
-            Text(match.expectedValue.draw.toStringAsFixed(3),
-                style: TextStyle(color: evDrawColor)),
-            Text(match.expectedValue.away.toStringAsFixed(3),
-                style: TextStyle(color: evAwayColor)),
-          ]),
-        ],
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('${(match.elo.eloProbs.home * 100).toStringAsFixed(1)}%'),
+                Text('${(match.elo.eloProbs.draw * 100).toStringAsFixed(1)}%'),
+                Text('${(match.elo.eloProbs.away * 100).toStringAsFixed(1)}%'),
+              ],
+            ),
+            const SizedBox(height: 8),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: (match.elo.eloProbs.home * 100).toInt(),
+                    child: Container(
+                      height: 8,
+                      color: Colors.green.shade500,
+                    ),
+                  ),
+                  Expanded(
+                    flex: (match.elo.eloProbs.draw * 100).toInt(),
+                    child: Container(
+                      height: 8,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  Expanded(
+                    flex: (match.elo.eloProbs.away * 100).toInt(),
+                    child: Container(
+                      height: 8,
+                      color: Colors.red.shade500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            // Odds
+            const Text(
+              'HUB Norsk Tipping Odds',
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          match.odds.home.toString(),
+                          style: const TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                        const Text(
+                          'Home',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          match.odds.draw.toString(),
+                          style: const TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                        const Text(
+                          'Draw',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          match.odds.away.toString(),
+                          style: const TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                        const Text(
+                          'Away',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+
+            // Expected Value
+            const Text(
+              'Expected Value per krone',
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(
+                        match.expectedValue.home.toStringAsFixed(3),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: _getEvColor(match.expectedValue.home),
+                        ),
+                      ),
+                      const Text(
+                        'Home',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(
+                        match.expectedValue.draw.toStringAsFixed(3),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: _getEvColor(match.expectedValue.draw),
+                        ),
+                      ),
+                      const Text(
+                        'Draw',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(
+                        match.expectedValue.away.toStringAsFixed(3),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: _getEvColor(match.expectedValue.away),
+                        ),
+                      ),
+                      const Text(
+                        'Away',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
